43d0d11f5dc1eee84355802a08480f2428a5f49e
Gunjan Jain <jain10gunjan@gmail.com>
2024-05-18 16:26:04 +0530

    Util Code Discussion

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,29 @@
+// const asyncHandler = (requestHandler) => {
+//     (req,res,next) => {
+//         Promise.resolve(requestHandler).catch((error) => next(error))
+//     }
+// }
+
+
+
+
+
+
+
+
+const asyncHandler = (fn) => {async (req,res,next) => {
+    try {
+        await fn(req,res,next)
+    } catch (error) {
+        res.status(error.code || 500).json({
+            success:false,
+            message:error.message
+        })
+    }
+}} //Higher Order Function 
+
+
+
+
+
+export default asyncHandler;
